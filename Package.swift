0d76281a3799438c154d5f6b4d5c8c88e925c1c8
TopScrech <sergei_saliukov@icloud.com>
2024-08-11 13:28:43 +0200

    QuickLookingView

--- Package.swift
+++ Package.swift
@@ -1,21 +1,19 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "QuickLooking",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // Products define the executables and libraries a package produces, making them visible to other packages
         .library(
             name: "QuickLooking",
-            targets: ["QuickLooking"]),
+            targets: ["QuickLooking"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "QuickLooking"),
-
+        // Targets are the basic building blocks of a package, defining a module or a test suite
+        // Targets can depend on other targets in this package and products from dependencies
+        .target(name: "QuickLooking")
     ]
 )
--- Sources/QuickLooking/QuickLooking.swift
+++ Sources/QuickLooking/QuickLooking.swift
@@ -1,2 +1,46 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+import QuickLook
+
+@available(iOS 13, *)
+public struct QuickLookView: UIViewControllerRepresentable {
+    let url: URL
+    
+    init(_ url: URL) {
+        self.url = url
+    }
+    
+    public func makeUIViewController(context: Context) -> QLPreviewController {
+        let controller = QLPreviewController()
+        controller.dataSource = context.coordinator
+        
+        return controller
+    }
+    
+    public func updateUIViewController(_ uiViewController: QLPreviewController, context: Context) {
+        
+    }
+    
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+}
+
+public final class Coordinator: NSObject, QLPreviewControllerDataSource {
+    private var parent: QuickLookView
+    
+    init(_ parent: QuickLookView) {
+        self.parent = parent
+    }
+    
+    public func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        1
+    }
+    
+    public func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        parent.url as QLPreviewItem
+    }
+}
