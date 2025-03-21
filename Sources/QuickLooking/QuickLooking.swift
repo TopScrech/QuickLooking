a7545a0a6953a08b64ac134877d7f15f0397be8d
TopScrech <sergei_saliukov@icloud.com>
2025-03-21 08:26:12 +0100

    -

--- Sources/QuickLooking/QuickLooking.swift
+++ Sources/QuickLooking/QuickLooking.swift
@@ -1,6 +1,3 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 import SwiftUI
 import QuickLook
 
@@ -19,14 +16,11 @@ public struct QuickLookView: UIViewControllerRepresentable {
         return controller
     }
     
-    public func updateUIViewController(_ uiViewController: QLPreviewController, context: Context) {
-        
-    }
+    public func updateUIViewController(_ uiViewController: QLPreviewController, context: Context) {}
     
     public func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
 }
 
 @available(iOS 13, *)
